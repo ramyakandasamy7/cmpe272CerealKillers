d8182a6a6b7f7f75ac832015502fbddbfcea8e0f
ramyakandasamy <ramyakandasamy7@gmail.com>
2019-11-07 11:01:30 -0800

    latest updates for timeOff

--- dataBase.js
+++ dataBase.js
@@ -1,28 +1,27 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema; 
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const timeOffSchema = new Schema( {
-    StartDate: {
-        type: Date,
-        default:null
-    },
-    endDate:  {
-        type: Date,
-        default:null
-    }
+const timeOffSchema = new Schema({
+  StartDate: {
+    type: Date,
+    default: null
+  },
+  endDate: {
+    type: Date,
+    default: null
+  }
 });
 
 const employeeSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    timeOffTotal: {
-        type: Number,
-        required: true
-    },
-    timeoffRequests:[timeOffSchema]
+  name: {
+    type: String,
+    required: true
+  },
+  timeOffTotal: {
+    type: Number,
+    required: true
+  },
+  timeoffRequests: [timeOffSchema]
 });
 
-
-module.exports = mongoose.model('Employees',employeeSchema);
+module.exports = mongoose.model("Employees", employeeSchema);
--- date.js
+++ date.js
@@ -1,32 +1,31 @@
 let getNumDays = function getNumWorkDays(startDate, endDate) {
-    var numWorkDays = 0;
-    var currentDate = new Date(startDate);
-    while (currentDate <= endDate) {
-        // Skips Sunday and Saturday
-        if (currentDate.getDay() !== 0 && currentDate.getDay() !== 6) {
-            numWorkDays++;
-        }
-        currentDate = currentDate.addDays(1);
+  var numWorkDays = 0;
+  var currentDate = new Date(startDate);
+  while (currentDate <= endDate) {
+    // Skips Sunday and Saturday
+    if (currentDate.getDay() !== 0 && currentDate.getDay() !== 6) {
+      numWorkDays++;
     }
-    console.log("number of work days is" + numWorkDays);
-    return numWorkDays;
-   
-}
-
-Date.prototype.addDays = function (days) {
-    var date = new Date(this.valueOf());
-    date.setDate(date.getDate() + days);
-    return date;
+    currentDate = currentDate.addDays(1);
+  }
+  console.log("number of work days is" + numWorkDays);
+  return numWorkDays;
 };
 
-Date.prototype.subtractDays = function (days) {
-    var date = new Date(this.valueOf());
-    date.setDate(date.getDate() - days);
-    return date;
-}
-Date.prototype.addHours = function(h) {
-    this.setTime(this.getTime() + (h*60*60*1000));
-    return this;
-  }
+Date.prototype.addDays = function(days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+};
 
-  module.exports = date;
+Date.prototype.subtractDays = function(days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() - days);
+  return date;
+};
+Date.prototype.addHours = function(h) {
+  this.setTime(this.getTime() + h * 60 * 60 * 1000);
+  return this;
+};
+
+module.exports = date;
